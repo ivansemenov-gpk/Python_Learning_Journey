410b831eb651150bb17dbc7bc72a2bc9272ce9c5
Vanyok <ivaneosrsemenov@gmail.com>
2026-04-14 19:40:22 +0200

    print("ИДИ НАХУЙ")

---
+++ first_program.py
@@ -0,0 +1,25 @@
+print("Я ебал меня сосали")
+print("Я", "ебал", "меня", "сосали")
+print("Мама Саши - она такая дура, я бы с удовольствием, шпенедербехнул")
+
+print("Как тебя зовут?")
+print("Привет,", input())
+
+variable_name = input()
+print("Вы ввели текст:", variable_name)
+print("Да ты козёл,", variable_name)
+petuh = input()
+print("Дима Побережный, ты -", petuh)
+
+name = "Дима Побережный"
+city = "Кривой Рог"
+print("Я знаю, что", name, "-", "шалава бля", "в городе", city)
+
+print("Как тебя зовут?")
+
+name = input()
+print("Привет,", name)
+
+name = "suck"
+print("Привет,", name)
+
